8449b791efaa989f1c40dc91c30d32af24a8976b
kyeung36220 <135488116+kyeung36220@users.noreply.github.com>
2024-12-21 11:39:06 -0500

    Finished Exercise 3

--- theodinproject/foundations/JS-foundations/03_reverseString/reverseString.js
+++ theodinproject/foundations/JS-foundations/03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    length = string.length - 1;
+    stringContainer = "";
+    for (let i=0; i <= length; i++) {
+        stringContainer = stringContainer.concat(string[length - i]);
+    }
+    return stringContainer;
 };
 
 // Do not edit below this line
