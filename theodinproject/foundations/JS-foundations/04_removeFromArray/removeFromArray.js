343166f3aa0fa468145eed6a1de28f6ab81572a4
kyeung36220 <135488116+kyeung36220@users.noreply.github.com>
2024-12-21 12:39:40 -0500

    Finished Exercise 4

--- theodinproject/foundations/JS-foundations/04_removeFromArray/removeFromArray.js
+++ theodinproject/foundations/JS-foundations/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let amountToRemove = arguments.length - 1;
+    let arrayLength = array.length;
+    for (let i=0; i <= arrayLength; i++) {
+        for (let j=1; j <= amountToRemove; j++) {
+            let a = array[i];
+            let b = arguments[j];
+            if (array[i] === arguments[j]) {
+                array.splice(i,1);
+                i = i - 1;
+            }
+        }
+    }
+    return array;
 };
 
+console.log(removeFromArray([1,2,3,4,5], 3, 2))
 // Do not edit below this line
 module.exports = removeFromArray;
