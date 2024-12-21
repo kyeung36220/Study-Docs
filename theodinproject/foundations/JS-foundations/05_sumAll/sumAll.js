f592e22f5470c761c97f86a9d618345e0ffb4328
kyeung36220 <135488116+kyeung36220@users.noreply.github.com>
2024-12-21 13:05:18 -0500

    Finished Exercise 5

--- theodinproject/foundations/JS-foundations/05_sumAll/sumAll.js
+++ theodinproject/foundations/JS-foundations/05_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    let maxInt = 0
+    let minInt = 0
 
+    //check if arguments are positive integers
+    for (let i=0; i <= arguments.length - 1; i++) {
+        if (Number.isInteger(arguments[i]) == false || arguments[i] < 0) {
+            return "ERROR"
+        }
+    }
+
+    if (int1 > int2) {
+        maxInt = int1
+        minInt = int2
+    }
+
+    else {
+        maxInt = int2
+        minInt = int1
+    }
+
+    sum = 0
+    for (let i = minInt; i <= maxInt; i++) {
+        sum += i
+    }
+    return sum
 };
+
+console.log(sumAll(10,[1,2,3]))
 
 // Do not edit below this line
 module.exports = sumAll;
