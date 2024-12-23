5db41d2f497622ab0066c294af1e1161ef141c47
kyeung36220 <135488116+kyeung36220@users.noreply.github.com>
2024-12-23 17:24:40 -0500

    Adding solutions for JS exercises 10 - 12

--- theodinproject/foundations/JS-foundations/10_fibonacci/fibonacci.js
+++ theodinproject/foundations/JS-foundations/10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(int) {
+    if (int < 0) {
+        return "OOPS"
+    }
+    else if (int == 0) {
+        return 0
+    }
+    else if (int == 1) {
+        return 1
+    }
 
+    fib = [0, 1]
+    for(i = 2; i <= int; i++) {
+        currentNumber = fib[i-2] + fib[i-1]
+        fib.push(currentNumber)
+    }
+    return fib[int]
 };
 
 // Do not edit below this line
--- theodinproject/foundations/JS-foundations/12_findTheOldest/findTheOldest.js
+++ theodinproject/foundations/JS-foundations/12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    arrCopy = arr.slice()
+    ages = arrCopy.map((person) => person.yearOfDeath !== undefined ? (person.yearOfDeath - person.yearOfBirth) : (new Date().getFullYear() - person.yearOfBirth))
 
+    maxAge = 0
+    maxAgeIndex = undefined
+    for (i = 0; i <= ages.length; i ++) {
+        if (ages[i] > maxAge) {
+            maxAge = ages[i]
+            maxAgeIndex = i
+        }
+    }
+    return arr[maxAgeIndex]
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
